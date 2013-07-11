73af6905bc5f6614351ff3053a6ea05f26b930ca
Robert Young <rhyoung214@gmail.com>
2013-07-11 03:37:45 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
- var express = require('express');
+var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  
-  response.send(fs.readFileSync('index.html'));
+  var buffer = new buffer ();
+  response.send(buffer.toString(fs.readFileSync('index.html', 'utf-8')));
 });
 
 var port = process.env.PORT || 5000;
